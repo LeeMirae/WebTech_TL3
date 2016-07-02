be3b69aaccde1c2fd98e3d9d8d2a9fabc308e7aa
Anastasia Simon <aivanenk@gmail.com>
2016-07-02 11:36:51 +0200

    Created draft of Model Classes. Not finished yet.

--- app/models/Game.java
+++ app/models/Game.java
@@ -4,12 +4,14 @@ import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
 import com.avaje.ebean.Model;
 
 import play.data.validation.Constraints.Required;
 
+@Entity
 public class Game extends Model {
 	
 	//Attributes
@@ -27,8 +29,28 @@ public class Game extends Model {
 	public Stadium stadium;
 	
 	
+	//Methods
+	
+	public static Finder<String, Game> find = new Finder<String, Game>(Game.class); 
 	
 	
+	//GetAll
+	public static List<Game> all(){
+		return find.all();
+	}
 	
+	//Constructor
+	public static void createGame(Game game,Stadium stadium){
+		game.save();
+	}
 	
+	//Delete
+	public void delete(Game game) {
+		game.delete();
+	}
+	
+	//Update
+	public void update(Game game){
+		game.update();
+	}
 }
--- app/models/Stadium.java
+++ app/models/Stadium.java
@@ -1,7 +1,62 @@
 package models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
 import com.avaje.ebean.Model;
 
+import play.data.validation.Constraints.Required;
+
+@Entity
 public class Stadium extends Model {
 
+	//Attributes
+	
+	@Required
+	String name;
+	
+	@Required
+	String adresse;
+	
+	@Required
+	int numberOfSeats;
+	
+	@Required
+	int numberOfexits;
+	
+	String sponsor;
+	
+	
+	@OneToMany(mappedBy="stadium",cascade=CascadeType.ALL)
+	public List<Game> games;
+	
+	
+	//Methods
+	
+	//Finder
+	public static Finder<String, Stadium> find = new Finder<String, Stadium>(Stadium.class);
+	
+	//GetAll
+	public static List<Stadium> all() {
+		return find.all();
+	}
+	
+	//Constructor
+	public static void createStadium(Stadium stadium){
+		stadium.save();
+	}
+	
+	//Delete
+	public void delete(String name){
+		find.ref(name).delete();
+	}
+	
+	//Update
+	public void update(String name){
+		find.ref(name).update();
+	}
+	
 }
