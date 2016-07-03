d6305cb513bff6db5d9132877325b4e92d5e691b
Felix Schweinfest <fschweinfest@hotmail.com>
2016-07-03 14:31:59 +0200

    merge

--- app/models/Game.java
+++ app/models/Game.java
@@ -3,17 +3,20 @@ package models;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 
 import com.avaje.ebean.Model;
 
 import play.data.validation.Constraints.Required;
 
+@Entity
 public class Game extends Model {
 	
 	//Attributes
 
+	@Id
+	public Long gameId;
+	
 	@Required
 	String homeTeamName;
 	
@@ -23,12 +26,45 @@ public class Game extends Model {
 	@Required
 	Date date;
 	
-	@ManyToOne (cascade = CascadeType.ALL)
-	public Stadium stadium;
+	@ManyToOne
+	private Stadium stadium;
 	
 	
+	//Finder
+	public static Finder<Long, Game> find = new Finder<Long, Game>(Game.class); 
 	
 	
+	//Get All Games
+	public static List<Game> all(){
+		return find.all();
+	}
+	
+	//Constructor
+	public static void createGame(Game game){
+		game.save();
+	}
+	
+	//Delete
+	public void delete(Long id) {
+		find.ref(id).delete();
+	}
+	
+	//Update
+	public void update(Game game){
+		game.update();
+	}
+
+	//Add a stadium
+	public void addStadium(Stadium st) {
+		if (stadium == null){
+		st.addGame(this);
+		}
+	}
+	
+	//Get stadium
+	public Stadium getStadium(){	
+		return this.stadium;
+	}
 	
 	
 }
--- app/models/Stadium.java
+++ app/models/Stadium.java
@@ -1,7 +1,13 @@
 package models;
 
+import java.util.List;
+import javax.persistence.*;
+
 import com.avaje.ebean.Model;
 
+import play.data.validation.Constraints.Required;
+
+@Entity
 public class Stadium extends Model {
 	private String name;
 	private String address;
@@ -27,4 +33,61 @@ public class Stadium extends Model {
 	// gameList.add(game);
 	// }
 
+	//Attributes
+	
+	@Id
+	public Long stadId;
+	
+	@Required
+	public String name;
+	
+	@Required
+	public String adresse;
+	
+	@Required
+	public int numberOfSeats;
+	
+	@Required
+	public int numberOfExits;
+	
+	public String sponsor;
+	
+	
+	@OneToMany(mappedBy="stadium",cascade=CascadeType.REMOVE)
+	public List<Game> games;
+	
+	
+	//Finder
+	public static Finder<Long, Stadium> find = new Finder<Long,Stadium>(Stadium.class);
+	
+	//get all stadiums
+	public static List<Stadium> all() {
+		return find.all();
+	}
+	
+	//Constructor
+	public static void createStadium(Stadium stadium){
+		stadium.save();
+	}
+	
+	//Delete
+	public static void delete(Long id){
+		find.ref(id).delete();
+	}
+	
+	//Update
+	public static void update(Stadium stadium){
+		stadium.update();
+	}
+	
+	//Add Game
+	 public void addGame(Game game){
+		this.games.add(game);
+		game.addStadium(this);
+	 }
+	 
+	 //Get all games
+	 public List<Game> getGames(){
+		 return this.games;
+	 }
 }
