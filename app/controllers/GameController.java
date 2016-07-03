dfe52c9706c03217843a7d1b1478bd37771c59e4
Felix Schweinfest <fschweinfest@hotmail.com>
2016-07-03 22:41:28 +0200

    implemented game controller and extended routes.conf

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,7 +1,53 @@
 package controllers;
 
+import javax.inject.Inject;
+
+import models.Game;
+import play.data.Form;
+import play.data.FormFactory;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class GameController extends Controller {
+
+	Game game = new Game();
+
+	@Inject
+	static FormFactory formFactory;
+
+	private static Form<Game> gameForm;
+
+	public static Result games() {
+		gameForm = formFactory.form(Game.class);
+		return ok(views.html.games.render(Game.all(), gameForm));
+	}
+
+	public Result createGame() {
+		gameForm = formFactory.form(Game.class);
+		Form<Game> filledForm = gameForm.bindFromRequest();
+		if (filledForm.hasErrors()) {
+			return badRequest(views.html.games.render(Game.all(), filledForm));
+		} else {
+			Game.createGame(filledForm.get());
+			return redirect(routes.GameController.games());
+		}
+	}
+
+	public Result readGames() {
+		return ok(views.html.games.render(Game.all()));
+	}
+
+	public Result deleteGame(Long gameId) {
+		game.delete(gameId);
+		return redirect(routes.GameController.games());
+	}
+
+	public Result findGame(Long gameId) {
+		return TODO;
+	}
+
+	public Result updateGame(Long gameId) {
+		return TODO;
+	}
 
 }
--- app/models/Stadium.java
+++ app/models/Stadium.java
@@ -1,7 +1,11 @@
 package models;
 
 import java.util.List;
-import javax.persistence.*;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import com.avaje.ebean.Model;
 
@@ -9,85 +13,60 @@ import play.data.validation.Constraints.Required;
 
 @Entity
 public class Stadium extends Model {
-	private String name;
-	private String address;
-	private int seats;
-	private int entrances;
-	private String sponsor;
 
-	// private List<Game> gameList;
-
-	public Stadium(String name, String address, int seats, int entrances) {
-		this.name = name;
-		this.address = address;
-		this.seats = seats;
-		this.entrances = entrances;
-
-	}
-
-	public void setSponsor(String sponsor) {
-		this.sponsor = sponsor;
-	}
-
-	// public void addPartie(Game game){
-	// gameList.add(game);
-	// }
-
-	//Attributes
-	
+	// Attributes
 	@Id
-	public Long stadId;
-	
+	public Long stadID;
+
 	@Required
 	public String name;
-	
+
 	@Required
 	public String address;
-	
+
 	@Required
 	public int numberOfSeats;
-	
+
 	@Required
 	public int numberOfExits;
-	
+
 	public String sponsor;
-	
-	
-	@OneToMany(mappedBy="stadium",cascade=CascadeType.REMOVE)
+
+	@OneToMany(mappedBy = "stadium", cascade = CascadeType.REMOVE)
 	public List<Game> games;
-	
-	
-	//Finder
-	public static Finder<Long, Stadium> find = new Finder<Long,Stadium>(Stadium.class);
-	
-	//get all stadiums
+
+	// Finder
+	public static Finder<Long, Stadium> find = new Finder<Long, Stadium>(Stadium.class);
+
+	// get all stadiums
 	public static List<Stadium> all() {
 		return find.all();
 	}
-	
-	//Constructor
-	public static void createStadium(Stadium stadium){
+
+	// Constructor
+	public static void createStadium(Stadium stadium) {
 		stadium.save();
 	}
-	
-	//Delete
-	public static void delete(Long id){
+
+	// Delete
+	public static void delete(Long id) {
 		find.ref(id).delete();
 	}
-	
-	//Update
-	public static void update(Stadium stadium){
+
+	// Update
+	public static void update(Stadium stadium) {
 		stadium.update();
 	}
-	
-	//Add Game
-	 public void addGame(Game game){
+
+	// Add Game
+	public void addGame(Game game) {
 		this.games.add(game);
 		game.addStadium(this);
-	 }
-	 
-	 //Get all games
-	 public List<Game> getGames(){
-		 return this.games;
-	 }
+	}
+
+	// Get all games
+	public List<Game> getGames() {
+		return this.games;
+	}
+
 }
